7573b45a5a92c52711370bc883de3d2d97727469
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-03-20 21:12:12 -0700

    Successfully sending and modifying user input data to app

--- app.py
+++ app.py
@@ -7,9 +7,15 @@ DATE CREATED:           Wednesday, March 20, 2019
 DATE LAST MODIFIED:     Wednesday, March 20, 2019
 """
 
-from flask import Flask
+from flask import Flask, request, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
-def main():
-    return "Hello, World!"
+def user_form():
+    return render_template("input-data.html")
+
+@app.route("/", methods=["POST"])
+def user_form_POST():
+    text = request.form["text"]
+    return "Your input is {}. How quaint!".format(text.upper())
