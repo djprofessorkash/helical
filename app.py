6c20c7b0688486ece0834e8c493b8f6e0177bc1b
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2019-03-21 11:46:26 -0700

    Improved Flask app to handle multiple POST requests and data entries

--- app.py
+++ app.py
@@ -23,14 +23,20 @@ def user_form():
 
 # Creates POST Route at Home Page with User Data Processing
 @app.route("/", methods=["POST"])
-def user_form_proc01():
-    text01 = request.form["proc01"]
-    proc01 = processor01.Processor01_NucleotideCounter(text01)
-    return proc01.render_response(proc01.nucleotide_counter())
-
-    # text02 = request.form["proc02"]
-    # proc02 = processor02.Processor02_DNATransriber(text02)
-    # return proc02.render_response(proc02.transcriber())
-
-# @app.route("/", methods=["POST"])
-# def user_form_proc02():
+def user_form_proc():
+    PROCESSORS = {
+        1: "Get Nucleotide Count",
+        2: "Convert to RNA",
+    }
+    if request.form["proc"] == PROCESSORS[1]:
+        if request.form["proc01"]:
+            proc01 = processor01.Processor01_NucleotideCounter(request.form["proc01"])
+            return proc01.render_response(proc01.nucleotide_counter())
+        else:
+            return "ERROR: Nucleotide Counts Processor did not receive any data."
+    elif request.form["proc"] == PROCESSORS[2]:
+        if request.form["proc02"]:
+            proc02 = processor02.Processor02_DNATransriber(request.form["proc02"])
+            return proc02.render_response(proc02.transcriber())
+        else:
+            return "ERROR: RNA Transcriber Processor did not receive any data."
